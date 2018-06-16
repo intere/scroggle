08c3e173143a2fcebd36af65e35f6088ee3bd8f3
Eric Internicola <intere@gmail.com>
2018-06-15 19:59:56 -0600

    Added a configuration and the values for all devices to render the overlays on the scroggle board.

--- Scroggle/Gameplay/Nodes/SCNGameScene+ClickableTiles.swift
+++ Scroggle/Gameplay/Nodes/SCNGameScene+ClickableTiles.swift
@@ -17,16 +17,13 @@ extension SCNGameScene {
         }
         tiles.removeAll()
 
-        let squareSize: CGFloat = 45
-        let stepSizeX: CGFloat = 75
-        let stepSizeY: CGFloat = 87.5
-
+        let config = TileConfiguration.currentConfiguration
         for row in -1...2 {
             for column in -2...1 {
-                let square = SKShapeNode(rectOf: CGSize(width: squareSize, height: squareSize))
+                let square = SKShapeNode(rectOf: CGSize(width: config.squareSize, height: config.squareSize))
                 square.fillColor = UIColor.red.withAlphaComponent(0.5)
-                let xPosition = scene.frame.midX - 37.5 + CGFloat(row) * stepSizeX
-                let yPosition = scene.frame.midY + 42.5 + CGFloat(column) * stepSizeY
+                let xPosition = scene.frame.midX - config.offsetX + CGFloat(row) * config.stepSizeX
+                let yPosition = scene.frame.midY + config.offsetY + CGFloat(column) * config.stepSizeY
                 square.position = CGPoint(x: xPosition, y: yPosition)
                 square.zPosition = 100
                 scene.addChild(square)
@@ -35,6 +32,7 @@ extension SCNGameScene {
         }
     }
 
+    /// Data structure to give you values to build the overlays
     private struct TileConfiguration {
         let squareSize: CGFloat
         let stepSizeX: CGFloat
@@ -50,12 +48,25 @@ extension SCNGameScene {
             self.offsetY = offsetY
         }
 
+        /// Gives you the overlay configuration for the current device
         static var currentConfiguration: TileConfiguration {
             if UIDevice.current.isiPhoneX {
                 return TileConfiguration(size: 45, sizeX: 75, sizeY: 87.5, offsetX: 37.5, offsetY: 42.5)
             }
-            
-            return TileConfiguration(size: 45, sizeX: 75, sizeY: 87.5, offsetX: 37.5, offsetY: 42.5)
+            if UIDevice.current.isiPhone5 {
+                return TileConfiguration(size: 45, sizeX: 77.5, sizeY: 77.5, offsetX: 37.5, offsetY: 37.5)
+            }
+            if UIDevice.current.isiPhone6Plus {
+                return TileConfiguration(size: 45, sizeX: 77.5, sizeY: 77.5, offsetX: 37.5, offsetY: 37.5)
+            }
+            if UIDevice.current.isiPhone6 {
+                return TileConfiguration(size: 45, sizeX: 77.5, sizeY: 77.5, offsetX: 37.5, offsetY: 37.5)
+            }
+            if UIDevice.current.isiPad {
+                return TileConfiguration(size: 45, sizeX: 77.5, sizeY: 87.5, offsetX: 37.5, offsetY: 42.5)
+            }
+
+            return TileConfiguration(size: 45, sizeX: 77.5, sizeY: 77.5, offsetX: 37.5, offsetY: 37.5)
         }
     }
 }
