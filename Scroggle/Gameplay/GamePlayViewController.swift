64ec2677a5ed52eea261798999bda55c39295c4f
Eric Internicola <intere@gmail.com>
2018-07-01 11:12:07 -0600

    Added various actions to the game.

--- Scroggle/Gameplay/GamePlayViewController.swift
+++ Scroggle/Gameplay/GamePlayViewController.swift
@@ -20,15 +20,18 @@ class GamePlayViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Hide the "Game Over" overlay.
         gameOverWidthConstraint.constant = 0
 
         seconds = GameContextProvider.instance.currentGame?.game.timer.timeType.seconds ?? 15
-
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self,
-                                     selector: #selector(updateTimer), userInfo: nil, repeats: true)
         scoreLabel.text = String(0)
 
         Notification.Scroggle.GameEvent.scoreUpdated.addObserver(self, selector: #selector(scoreUpdated))
+
+        // TODO: Delay this after an "introduction animation":
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,
+                                     selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
 }
 
@@ -49,11 +52,7 @@ private extension GamePlayViewController {
         timerLabel.text = seconds.timeString
 
         guard seconds > 0 else {
-            timerLabel.textColor = .red
-            timer?.invalidate()
-            showGameOver()
-            Notification.Scroggle.GameEvent.gameEnded.notify()
-            return
+            return endGame()
         }
 
         if seconds <= 10 {
@@ -71,9 +70,19 @@ private extension GamePlayViewController {
         }
     }
 
+    /// Shows the "Game Over" overlay (animates it in).
     func showGameOver() {
         UIView.animate(withDuration: 0.5) {
             self.gameOverWidthConstraint.constant = self.skView.frame.width
         }
+    }
+
+    /// Ends the game, and handles (or delgates) all associated logic.
+    func endGame() {
+        GameContextProvider.instance.currentGame?.gameState = .done
+        timerLabel.textColor = .red
+        timer?.invalidate()
+        showGameOver()
+        Notification.Scroggle.GameEvent.gameEnded.notify()
     }
 }
