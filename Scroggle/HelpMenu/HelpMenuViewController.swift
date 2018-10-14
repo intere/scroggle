aabaf8a3830de3b4e6854b1acf010293d739aa15
Eric Internicola <intere@gmail.com>
2018-10-14 09:34:49 -0600

    Added the "General help" button.

--- Scroggle/HelpMenu/HelpMenuViewController.swift
+++ Scroggle/HelpMenu/HelpMenuViewController.swift
@@ -28,9 +28,15 @@ extension HelpMenuViewController: MenuBuilding {
 
     func buildMenu() -> MenuInfo? {
         return MenuInfo(title: "Help", buttons: [
+            ButtonCellInfo(title: "General Help", action: { [weak self] in
+                SoundProvider.instance.playMenuSelectionSound()
+                let url = "https://intere.github.io/scroggle-support/#/help"
+                self?.navigationController?.pushViewController(WebViewController.loadFromStoryboard(url: url), animated: true)
+            }),
             ButtonCellInfo(title: "Goals + Rules", action: { [weak self] in
                 SoundProvider.instance.playMenuSelectionSound()
-                self?.navigationController?.pushViewController(WebViewController.loadFromStoryboard(url: "https://intere.github.io/scroggle-support/#/rules"), animated: true)
+                let url = "https://intere.github.io/scroggle-support/#/rules"
+                self?.navigationController?.pushViewController(WebViewController.loadFromStoryboard(url: url), animated: true)
             }),
             ButtonCellInfo(title: "Email support", action: { [weak self] in
                 SoundProvider.instance.playMenuSelectionSound()
